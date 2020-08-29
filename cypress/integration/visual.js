162a154ea09004820990efb693ee967af23696aa
harmiksardar <harmik.sardar@gmail.com>
2020-08-29 16:48:04 +1000

    testing most used viewport

--- cypress/integration/visual.js
+++ cypress/integration/visual.js
@@ -1,18 +1,18 @@
 describe('rathdowneyogaroom test', () => {
   it('yoga site should work', () => {
-    cy.visit('https://rathdowneyogaroom.com.au');
+    cy.visit('https://rathdowneyogaroom.com.au')
     cy.eyesOpen({
       appName: 'My Yoga Website',
-      testName: 'Testing 800x1200 screensize',
-      browser: { width: 800, height: 1200 },
+      testName: 'Testing 1680x1050 screensize',
+      browser: { width: 1680, height: 1050 }
     });
-      cy.eyesCheckWindow('Home screen');
+      cy.eyesCheckWindow('Home screen')
     //cy.get('button').click();
     //cy.eyesCheckWindow('Click!');
     cy.get(':nth-child(3) > .button').click()
     cy.url().should('include', 'classes')
 
-      cy.eyesCheckWindow('Classes screen');
+      cy.eyesCheckWindow('Classes screen')
 
     cy.eyesClose();
   });
