ffcd7f2afa1d1b3ed7752bad2d733d1857c7b95b
harmiksardar <harmik.sardar@gmail.com>
2020-09-01 22:01:04 +1000

    multi browser/viewport/device coverageplus clean code

--- cypress/integration/visual.js
+++ cypress/integration/visual.js
@@ -1,27 +1,30 @@
-describe('rathdowneyogaroom test', () => {
-  it('yoga site should work', () => {
-    cy.visit('https://rathdowneyogaroom.com.au')
+describe('YogaRoom E2E Test Suite', () => {
+
+  beforeEach( () => {
     cy.eyesOpen({
-      appName: 'My Yoga Website',
-      testName: 'Cross Browser + Device',
-      browser: [{ width: 1680, height: 1050, name: 'firefox' },
-                { width: 1024, height: 768, name: 'chrome' },
-                { deviceName: 'iPhone X', screenOrientation: 'landscape', name: 'chrome' }
-               ]
-    });
+      appName: 'My Yoga App',
+      testName: 'Cross Browser/Viewport/Device',
+      browser: [{ width: 1366, height: 768, name: 'firefox' },
+                { width: 1920, height: 1080, name: 'chrome' },
+                { width: 1440, height: 900, name: 'chrome-one-version-back' },
+                { deviceName: 'iPhone XR', screenOrientation: 'landscape', name: 'chrome' }]
+             })
+           })
 
-      cy.eyesCheckWindow('Home screen')
+  afterEach( () => {
+    cy.eyesClose()
+    })
 
+  it('YogaRoom E2E Test Case', () => {
+    cy.visit('https://rathdowneyogaroom.com.au')
+    cy.get('#text01').should('contain', 'RATHDOWNE YOGA ROOM')
+    //cy.eyesCheckWindow('Home screen')
     cy.get(':nth-child(3) > .button').click()
     cy.url().should('include', 'classes')
-
-      cy.eyesCheckWindow('Classes screen')
-
+    cy.eyesCheckWindow('Classes screen')
     cy.get('#buttons06 > li > .button').click()
     cy.url().should('not.include', 'classes')
+    cy.eyesCheckWindow('Back to Home screen')
+  })
 
-      cy.eyesCheckWindow('Back to Home screen')
-
-    cy.eyesClose()
-  });
-});
+})
